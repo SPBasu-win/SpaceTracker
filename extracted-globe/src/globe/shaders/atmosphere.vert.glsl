68bcf1c6d35d73262dc121bc5227d190f00fe533
Swastik Prasad Basu <swastikbasu567@gmail.com>
2026-06-14 11:49:26 +0530

    Deleted lax files

--- extracted-globe/src/globe/shaders/atmosphere.vert.glsl
+++
@@ -1,9 +0,0 @@
-varying vec3 vNormal;
-varying vec3 vViewDir;
-
-void main() {
-  vNormal = normalize(normalMatrix * normal);
-  vec4 worldPos = modelViewMatrix * vec4(position, 1.0);
-  vViewDir = normalize(-worldPos.xyz);
-  gl_Position = projectionMatrix * worldPos;
-}
